399c5b94ef691bb5ce24ea09aaf78e88d81b554c
Tom Regan <code.tom.regan@gmail.com>
2016-03-28 00:09:41 +0100

    Use mucky magic to create the lazy loaded functions

--- libexec/mvn.bash
+++ libexec/mvn.bash
@@ -2,18 +2,17 @@
 # and: http://johannes.jakeapp.com/blog/category/fun-with-linux/200901/maven-colorized
 # Colorize Maven Output
 # alias maven="command mvn"
-color_maven() {
-  $* | LANG=C sed -e 's/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/[1;32mTests run: \1[0m, Failures: [0;31m\2[0m, Errors: [0;33m\3[0m, Skipped: [0;34m\4[0m/g' \
-    -e 's/\([^at]*\)\(at\ [A-Za-z]*\..*\)/\1[0;31m\2[0m/g' \
-    -e 's/^\([A-Za-z]*\..*\)/[0;31m\1[0m/g' \
-    -e 's/\(\[WARN\].*\)/[0;33m\1[0m/g' \
-    -e 's/\(WARN.*\)/[0;33m\1[0m/g' \
-    -e 's/\(\[ERROR\].*\)/[0;31m\1[0m/g' \
-    -e 's/\(BUILD FAILURE.*\)/[0;31m\1[0m/g' \
-    -e 's/\(FAILURE!.*\)/[0;31m\1[0m/g' \
-    -e 's/\(BUILD SUCCESS.*\)/[0;37m\1[0m/g' \
-    -e 's/\(SUCCESS.*\)/[0;37m\1[0m/g' 
-}
+
+$* | LANG=C sed -e 's/Tests run: \([^,]*\), Failures: \([^,]*\), Errors: \([^,]*\), Skipped: \([^,]*\)/[1;32mTests run: \1[0m, Failures: [0;31m\2[0m, Errors: [0;33m\3[0m, Skipped: [0;34m\4[0m/g' \
+         -e 's/\([^at]*\)\(at\ [A-Za-z]*\..*\)/\1[0;31m\2[0m/g' \
+         -e 's/^\([A-Za-z]*\..*\)/[0;31m\1[0m/g' \
+         -e 's/\(\[WARN\].*\)/[0;33m\1[0m/g' \
+         -e 's/\(WARN.*\)/[0;33m\1[0m/g' \
+         -e 's/\(\[ERROR\].*\)/[0;31m\1[0m/g' \
+         -e 's/\(BUILD FAILURE.*\)/[0;31m\1[0m/g' \
+         -e 's/\(FAILURE!.*\)/[0;31m\1[0m/g' \
+         -e 's/\(BUILD SUCCESS.*\)/[0;37m\1[0m/g' \
+         -e 's/\(SUCCESS.*\)/[0;37m\1[0m/g'
 
 alias mvn="color_maven mvn"
 alias mvn2="color_maven /usr/local/bin/mvn"
--- libexec/sbtenv.bash
+++ libexec/sbtenv.bash
@@ -1,24 +1,19 @@
-function sbtenv() {
-    eval export PATH="/Users/tom/.sbtenv/shims:${PATH}"
-    export SBTENV_SHELL=bash
-    source '/usr/local/Cellar/sbtenv/0.0.9/libexec/../completions/sbtenv.bash'
-    sbtenv rehash 2> /dev/null
-    unset -f sbtenv
-    sbtenv() {
-        local command
-        command="$1"
-        if [ "$#" -gt 0 ]; then
-            shift
-        fi
+eval export PATH="/Users/tom/.sbtenv/shims:${PATH}"
+export SBTENV_SHELL=bash
+source '/usr/local/Cellar/sbtenv/0.0.9/libexec/../completions/sbtenv.bash'
+sbtenv rehash 2> /dev/null
+unset -f sbtenv
+sbtenv() {
+    local command
+    command="$1"
+    if [ "$#" -gt 0 ]; then
+        shift
+    fi
 
-        case "$command" in
-            rehash|shell)
-                eval "`sbtenv "sh-$command" "$@"`";;
-            * )
-                command sbtenv "$command" "$@";;
-        esac
-    }
-    sbtenv $@
+    case "$command" in
+        rehash|shell)
+            eval "`sbtenv "sh-$command" "$@"`";;
+        * )
+            command sbtenv "$command" "$@";;
+    esac
 }
-
-export -f sbtenv
