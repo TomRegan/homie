6992720f366faf991db25cd50cc2a4f23c758cba
Tom Regan <tom.regan@worldpay.com>
2019-09-20 12:11:06 +0100

    update settings for new laptop

--- include/aliases.bash
+++ include/aliases.bash
@@ -1,6 +1,6 @@
 # Enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    test -r ~/.dircolors && eval "$(/usr/bin/dircolors -b ~/.dircolors)" || eval "$(/usr/bin/dircolors -b)"
     alias ls='ls --color=auto'
     #alias dir='dir --color=auto'
     #alias vdir='vdir --color=auto'
@@ -9,13 +9,14 @@ if [ -x /usr/bin/dircolors ]; then
     alias egrep='egrep --color=auto'
 fi
 # ls
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias ls='ls -G'
-alias ll='ls -al'
+LS_OPTS="--color=yes"
+alias ll='ls ${LS_OPTS} -alF'
+alias la='ls ${LS_OPTS} -A'
+alias l='ls ${LS_OPTS} -CF'
+alias ls='ls ${LS_OPTS} -G'
+alias ll='ls ${LS_OPTS} -al'
 # job control
 alias j=jobs
 # editors
-alias vi=vim
-alias emacs='emacsclient -t'
+#alias vi=vim
+#alias emacs='emacsclient -t'
